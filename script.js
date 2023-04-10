62e86f8397fd97f84d2beb1734933f725d6e3c8d
Saiisshh <118446344+Saiissh@users.noreply.github.com>
2023-04-10 23:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+	console.log("Hello, my name is "+this.name+" , I am "+this.age+" years old.")
+	}
+}
+
+function Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
+	this.name=name;
+		this.age=age;
+		this,jobTitle=jobTitle;
+	}
+	
+}
 
 // Do not change code below this line
 window.Person = Person;
