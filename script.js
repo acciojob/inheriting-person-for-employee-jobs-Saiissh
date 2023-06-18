b5042362dc6f3fbfb1d1d5464e23d41c236864ea
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-18 15:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// complete this js code
+// Person constructor
 function Person(name, age) {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-
-	greet(){
-	console.log("Hello, my name is "+this.name+" , I am "+this.age+" years old.")
-	}
+  this.name = name;
+  this.age = age;
 }
 
+// Method added to Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee constructor inheriting from Person
 function Employee(name, age, jobTitle) {
-	constructor(name,age,jobTitle){
-	this.name=name;
-		this.age=age;
-		this,jobTitle=jobTitle;
-	}
-	
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Inheriting Person prototype into Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Method added to Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
