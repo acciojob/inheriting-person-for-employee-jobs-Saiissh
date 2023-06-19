1c04599762441a89f30190941cf232a6d2a96938
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-19 18:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+// Person constructor
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-Person.prototype.greet = function() {
+// Person prototype method
+Person.prototype.greet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
-// Employee constructor inheriting from Person
+// Employee constructor
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inheriting Person prototype into Employee prototype
+// Inherit the Person prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Method added to Employee prototype
-Employee.prototype.jobGreet = function() {
+// Employee prototype method
+Employee.prototype.jobGreet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
+// Usage example
+const person = new Person('John', 30);
+person.greet(); // Output: Hello, my name is John, I am 30 years old.
+
+const employee = new Employee('Jane', 25, 'Manager');
+employee.greet(); // Output: Hello, my name is Jane, I am 25 years old.
+employee.jobGreet(); // Output: Hello, my name is Jane, I am 25 years old, and my job title is Manager.
